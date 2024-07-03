48b682a10fe163f9c5f7151ff6b256b441cae5f1
Mukul <mukulkriplani09@gmail.com>
2024-07-04 00:30:27 +0530

    localtoeverywhere

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/dynamic",{
+// mongoose.connect("mongodb://localhost:27017/dynamic",{
+mongoose.connect("mongodb+srv://the_mukul15:CNuKLNkPStTpKDyL@cluster0.70uifbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
 
 }).then(()=>{
     console.log("Connected");
